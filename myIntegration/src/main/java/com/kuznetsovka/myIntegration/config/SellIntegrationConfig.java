ab3dc42f7c655119d28ade3f45db89341f487130
Kirill Kuznetsov <kuznechik.86@mail.ru>
2020-10-25 22:13:09 +0300

    Рабочая версия без БД

--- myIntegration/src/main/java/com/kuznetsovka/myIntegration/config/SellIntegrationConfig.java
+++ myIntegration/src/main/java/com/kuznetsovka/myIntegration/config/SellIntegrationConfig.java
@@ -1,9 +1,7 @@
 package com.kuznetsovka.myIntegration.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.integration.channel.DirectChannel;
 
 @Configuration
 @ImportResource("integration/http-sells-gateway.xml")
