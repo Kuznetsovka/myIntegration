fca4aae7a37ef401161657b117c2703f224fac79
Kirill Kuznetsov <kuznechik.86@mail.ru>
2020-10-25 16:06:53 +0300

    Initial commit

---
+++ myIntegration/src/main/java/com/kuznetsovka/myIntegration/MyIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.kuznetsovka.myIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyIntegrationApplication.class, args);
+	}
+
+}
